9664709a8688eb5850a7c5b8df5543b12aa051d7
Jorge Yau <jyau01@citymail.cuny.edu>
2015-04-10 09:12:46 -0400

    #2 - Created TimeStampedModel in core app

--- vanilla/apps/core/models.py
+++ vanilla/apps/core/models.py
@@ -1,3 +1,17 @@
+"""
+Description:
+This app contains core application models that are meant
+to be reused throughout an entire project.
+"""
 from django.db import models
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    """
+    An abstract class model that provides the auto-updating
+    fields: (1) created and (2) modified.
+    """
+    created  = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- vanilla/urls.py
+++ vanilla/urls.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 urlpatterns = patterns('',
 
     # Grappelli jazzy admin skin
-    url(r'^grappelli/', include('grappelli.urls')),
+    # url(r'^grappelli/', include('grappelli.urls')),
 
     # Obscure the admin URL with a long random string.
-    url(r'^django-admin/', include(admin.site.urls)),
+    # url(r'^django-admin/', include(admin.site.urls)),
 
     # Examples:
     # url(r'^$', 'vanilla.views.home', name='home'),
