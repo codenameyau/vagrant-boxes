fc3c9c554abf0d201a8c1b84ca309addcffb9605
Jorge Yau <jyau01@citymail.cuny.edu>
2015-04-19 20:17:02 -0400

    Resolves #7 - Added logging configuration to production settings

--- vanilla/settings/production.py
+++ vanilla/settings/production.py
@@ -42,3 +42,35 @@ CACHES = {
 ################################################################
 # PRODUCTION LOGGING SETTINGS
 ################################################################
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'vanilla.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'vanilla': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
